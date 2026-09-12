178eabe9765611d2f3b7a68a1298328fa0a3c7c3
GeorgeTR <bjornmortenson@gmail.com>
2026-09-12 11:38:26 -0500

    Change help to reflect docopt standard

--- recycle-bin.c
+++ recycle-bin.c
@@ -24,8 +24,9 @@ int mainNoCRT() {
       }
 
       if (lstrcmpW(argv[1], L"--help") == 0) {
+         // usage description follows standard described at http://docopt.org/
          fputsWin("\n  Move files and folders to the recycle bin\n\n"
-         "  Usage: recycle <path> [...]\n", STD_OUTPUT_HANDLE);
+         "  Usage: recycle <path>...\n", STD_OUTPUT_HANDLE);
          ExitProcess(0);
       }
    }
